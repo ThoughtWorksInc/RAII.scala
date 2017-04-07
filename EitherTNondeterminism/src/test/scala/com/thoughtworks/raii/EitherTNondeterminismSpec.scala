1e8e8cb76bae68c6f33444323a846eb6d9c4f8de
Yang Bo <atryyang@thoughtworks.com>
2017-04-07 09:23:06 +0800

    File name should be upper camel case

--- EitherTNondeterminism/src/test/scala/com/thoughtworks/raii/EitherTNondeterminismSpec.scala
+++ EitherTNondeterminism/src/test/scala/com/thoughtworks/raii/EitherTNondeterminismSpec.scala
@@ -5,6 +5,6 @@ import org.scalatest.{AsyncFreeSpec, FreeSpec, Inside, Matchers}
 /**
   * Created by 张志豪 on 2017/4/6.
   */
-class eitherTNondeterminismSpec extends AsyncFreeSpec with Matchers with Inside{
+class EitherTNondeterminismSpec extends AsyncFreeSpec with Matchers with Inside{
 
 }
