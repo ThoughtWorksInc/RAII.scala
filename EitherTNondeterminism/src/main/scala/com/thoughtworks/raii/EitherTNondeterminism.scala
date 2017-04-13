1af0eed3366ae6378199e4a21a9bdeecbfe138e2
Yang Bo <atryyang@thoughtworks.com>
2017-04-13 12:40:44 +0800

    Support Applictive for Parallel EitherT

--- EitherTNondeterminism/src/main/scala/com/thoughtworks/raii/EitherTNondeterminism.scala
+++ EitherTNondeterminism/src/main/scala/com/thoughtworks/raii/EitherTNondeterminism.scala
@@ -1,12 +1,46 @@
 package com.thoughtworks.raii
 
 import scala.language.higherKinds
-import scalaz.{Apply, EitherT, Monoid, Nondeterminism, Reducer, \/}
+import scalaz.Tags.Parallel
+import scalaz.{-\/, @@, Applicative, Apply, EitherT, Monoid, Nondeterminism, Reducer, Semigroup, \/, \/-}
+import scalaz.syntax.all._
 
 object EitherTNondeterminism {
+
+  implicit def eitherTParallelApplictive[F[_], L](
+      implicit F0: Applicative[Lambda[R => F[R] @@ Parallel]],
+      semigroup: Semigroup[L]): Applicative[Lambda[R => EitherT[F, L, R] @@ Parallel]] =
+    new Applicative[Lambda[R => EitherT[F, L, R] @@ Parallel]] {
+      override def point[R](r: => R): EitherT[F, L, R] @@ Parallel = {
+        val fa: F[L \/ R] @@ Parallel = F0.point(\/-(r))
+        Parallel(new EitherT(Parallel.unwrap(fa)))
+      }
+
+      override def ap[A, B](fa: => EitherT[F, L, A] @@ Parallel)(
+          f: => EitherT[F, L, A => B] @@ Parallel): EitherT[F, L, B] @@ Parallel = {
+        Parallel(
+          new EitherT(Parallel.unwrap(F0.apply2(Parallel(Parallel.unwrap(fa).run), Parallel(Parallel.unwrap(f).run)) {
+            (eitherA: L \/ A, eitherF: L \/ (A => B)) =>
+              eitherA match {
+                case \/-(a) =>
+                  eitherF match {
+                    case \/-(f) => \/-(f(a))
+                    case left @ -\/(_) => left
+                  }
+                case leftA @ -\/(la) =>
+                  eitherF match {
+                    case \/-(_) => leftA
+                    case leftF @ -\/(lf) =>
+                      -\/(la |+| lf)
+                  }
+              }
+          })))
+      }
+    }
+
   implicit def eitherTNondeterminism[F[_], L](implicit F0: Nondeterminism[F]): Nondeterminism[EitherT[F, L, ?]] =
     new EitherTNondeterminism[F, L] {
-      val F = Nondeterminism[F]
+      val F = F0
     }
 }
 
@@ -25,8 +59,7 @@ private[raii] trait EitherTNondeterminism[F[_], L] extends Nondeterminism[Either
 
   override def point[A](a: => A): EitherT[F, L, A] = EitherT.eitherTMonad[F, L].point(a)
 
-  override def reduceUnordered[A, M](fs: Seq[EitherT[F, L, A]])(
-      implicit R: Reducer[A, M]): EitherT[F, L, M] = {
+  override def reduceUnordered[A, M](fs: Seq[EitherT[F, L, A]])(implicit R: Reducer[A, M]): EitherT[F, L, M] = {
     import R.monoid
     val AE = Apply[L \/ ?]
     val RR: Reducer[L \/ A, L \/ M] =
