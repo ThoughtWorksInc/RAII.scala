608db984eb26b1ba4299f3fc28e1442e825c3228
Yang Bo <atryyang@thoughtworks.com>
2017-05-03 11:42:40 +0800

    Add tests

---
+++ ownership/src/test/scala/com/thoughtworks/raii/ownershipSpec.scala
@@ -0,0 +1,45 @@
+package com.thoughtworks.raii
+
+import java.io.{CharArrayWriter, Closeable, Writer}
+
+import com.thoughtworks.raii.ownership._
+import com.thoughtworks.raii.ownership.implicits._
+import org.scalatest.{FreeSpec, Matchers}
+
+/**
+  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+  */
+final class ownershipSpec extends FreeSpec with Matchers {
+
+  final class MyOwner(gift: Nothing Owned Writer) extends Closeable {
+    val myWriter: this.type Owned Writer = gift
+
+    def foo(mine: this.type Owned Writer): Unit = {}
+
+    override def close(): Unit = {
+      myWriter.close()
+    }
+  }
+
+  "Given a resource owned by an owner" - {
+    val owner1 = new MyOwner(Owned(new CharArrayWriter()))
+
+    "Then the resource should be able to borrow" in {
+      def scope(notMine: Borrowing[Writer]) = {}
+      scope(owner1.myWriter)
+    }
+
+    "And the resource should be used for the owner" in {
+      owner1.foo(owner1.myWriter)
+    }
+
+    "And the resource should not be owned by another owned" in {
+      "val owner2 = new MyOwner(owner1.myWriter)" shouldNot typeCheck
+    }
+
+    "And the resource should not be used for another owner" in {
+      val owner3 = new MyOwner(Owned(new CharArrayWriter()))
+      "owner3.foo(owner1.myWriter)" shouldNot typeCheck
+    }
+  }
+}
