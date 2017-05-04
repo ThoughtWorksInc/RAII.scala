ba634cc0fc14041af55bca747889c4313f59a144
Yang Bo <atryyang@thoughtworks.com>
2017-05-04 17:33:00 +0800

    Add Scaladoc for ownership library

--- ownership/src/main/scala/com/thoughtworks/raii/ownership.scala
+++ ownership/src/main/scala/com/thoughtworks/raii/ownership.scala
@@ -3,7 +3,26 @@ package com.thoughtworks.raii
 import scala.language.higherKinds
 import scala.language.implicitConversions
 
-/**
+/** The name space for types that models ownership of resources.
+  *
+  * @example {{{
+  * import com.thoughtworks.raii.ownership._
+  * import com.thoughtworks.raii.ownership.implicits._
+  *
+  * class TheOwner extends AutoCloseable {
+  *   private val myStream: this.type Owned InputStream = this.own(new FileInputStream("foo.txt"))
+  *   def close() = myStream.close()
+  *   def withMyStream(f: Borrowing[InputStream] => Unit) = {
+  *     f(myStream)
+  *   }
+  * }
+  *
+  * val owner = new TheOwner
+  * owner.withMyStream { inputStream: Borrowing[InputStream] =>
+  *   println(inputStream.read())
+  * }
+  * }}}
+  *
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 object ownership {
@@ -26,16 +45,48 @@ object ownership {
     @inline override def garbageCollectable[A](a: A): A = a
   }
 
+  /** An resource `A` that is only available in certain scope.
+    *
+    * For example, a `FileInputStream` that will be automatically `close`d when exiting a scope.
+    *
+    * @note You should not store [[Scoped]] resource for out-of-scope usage.
+    *
+    * @example {{{
+    * val outOfScopeStore = ArrayBuffer.empty[FileInputStream]
+    * def read(stream: Scoped[FileInputStream]) = {
+    *   // OK! You can use a scoped resource inside `read` method
+    *   stream.read();
+    *
+    *   // Wrong! A scoped resource should not be stored.
+    *   outOfScopeStore += stream
+    *
+    *   // Wrong! A scoped resource is managed by another owner. It should not be closed inside the scope.
+    *   stream.close();
+    * }
+    * }}}
+    */
   type Scoped[+A] = opacityTypes.Scoped[A]
 
+  /** A [[Scoped]] resource managed by `Owner`.
+    *
+    * It's `Owner`'s responsibility to `close` or `release` the underlying resource when exiting the scope.
+    */
   type Owned[+Owner <: Singleton, +A] = opacityTypes.Owned[Owner, A]
 
   object Owned {
 
+    /** Declares ownership between `a` and `Owner`. */
     def apply[Owner <: Singleton, A](a: A) = opacityTypes.own[Owner, A](a)
   }
 
+  /** An object `A` that is managed by garbage collector. */
   type GarbageCollectable[+A] = opacityTypes.GarbageCollectable[A]
+
+  /** An object `A` that may be scoped.
+    *
+    * A method that accepts a [[Borrowing]] parameter must follows all instructions on [[Scoped]] resource.
+    * However, a method that provides a [[Borrowing]] may create it either from [[GarbageCollectable]] or from [[Owned]].
+    */
   type Borrowing[+A] = opacityTypes.Borrowing[A]
 
   trait Move[OldOwner <: Singleton, A] {
@@ -47,9 +98,11 @@ object ownership {
   }
 
   final class OwnOps[Owner <: Singleton] {
+    /** Declares ownership between `a` and `Owner`. */
     def own[A](a: A): Owner Owned A = opacityTypes.own(a)
   }
 
+  /** Marks `a` as garbage collectable. */
   def garbageCollectable[A](a: A): GarbageCollectable[A] = opacityTypes.garbageCollectable(a)
 
   object implicits {
