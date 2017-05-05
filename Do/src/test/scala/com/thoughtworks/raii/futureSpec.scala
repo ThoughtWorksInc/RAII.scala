45905590b881a7f499588449e12d9127e927f7c9
Yang Bo <atryyang@thoughtworks.com>
2017-05-05 23:23:46 +0800

    Test Do.autoReleaseDependencies

--- Do/src/test/scala/com/thoughtworks/raii/futureSpec.scala
+++ Do/src/test/scala/com/thoughtworks/raii/futureSpec.scala
@@ -3,7 +3,12 @@ package com.thoughtworks.raii
 import java.io.StringWriter
 
 import com.thoughtworks.raii.future.Do
-import org.scalatest.{FreeSpec, Matchers}
+import com.thoughtworks.raii.future.Do._
+
+import scalaz.syntax.all._
+import org.scalatest.{Assertion, FreeSpec, Matchers}
+
+import scala.concurrent.Promise
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
@@ -18,4 +23,41 @@ final class futureSpec extends FreeSpec with Matchers {
     "Do.run(Do.now('foo))" should compile
   }
 
+  "Given a scoped resource" - {
+    var isSourceClosed = false
+    val source = Do.scoped(new AutoCloseable {
+      isSourceClosed should be(false)
+      override def close(): Unit = {
+        isSourceClosed should be(false)
+        isSourceClosed = true
+      }
+    })
+    "And flatMap the resource to an new autoReleaseDependencies resource" - {
+      var isResultClosed = false
+      val result = autoReleaseDependencies(source.flatMap { sourceCloseable =>
+        Do.scoped(new AutoCloseable {
+          isResultClosed should be(false)
+          override def close(): Unit = {
+            isResultClosed should be(false)
+            isResultClosed = true
+          }
+        })
+      })
+      "When map the new resource" - {
+        "Then dependency resource should have been released" in {
+          val p = Promise[Assertion]
+          Do.run(result.map { r =>
+              isSourceClosed should be(true)
+              isResultClosed should be(false)
+            })
+            .unsafePerformAsync { either =>
+              isSourceClosed should be(true)
+              isResultClosed should be(true)
+              p.complete(scalaz.std.`try`.fromDisjunction(either))
+            }
+          p.future
+        }
+      }
+    }
+  }
 }
