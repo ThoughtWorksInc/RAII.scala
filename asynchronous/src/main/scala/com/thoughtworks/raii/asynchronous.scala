2f635d1b039625a395b233a7b95aa4efcbbf643c
Yang Bo <pop.atry@gmail.com>
2017-08-23 13:34:21 +0800

    Add `Do.resource` method

--- asynchronous/src/main/scala/com/thoughtworks/raii/asynchronous.scala
+++ asynchronous/src/main/scala/com/thoughtworks/raii/asynchronous.scala
@@ -19,6 +19,7 @@ import com.thoughtworks.continuation.{UnitContinuation, _}
 import com.thoughtworks.raii.asynchronous.Do.scoped
 import com.thoughtworks.raii.shared._
 
+import scala.util.control.NonFatal
 import scalaz.syntax.all._
 import scalaz.std.anyVal._
 
@@ -164,6 +165,16 @@ object asynchronous {
     * @define garbageCollected `Value` must be a garbage-collected type that does not hold native resource.
     */
   object Do {
+    def resource[A](resource: => Resource[UnitContinuation, A]): Do[A] = {
+      val tryResource = try {
+        val Resource(a, monadicClose) = resource
+        Resource(Success(a), monadicClose)
+      } catch {
+        case NonFatal(e) =>
+          Resource(Failure(e), UnitContinuation.now(()))
+      }
+      Do(TryT(ResourceT(UnitContinuation.now(tryResource))))
+    }
 
     def apply[Value](tryT: TryT[ResourceT[UnitContinuation, `+?`], Value]): Do[Value] = {
       opacityTypes.fromTryT(tryT)
